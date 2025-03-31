91aad3d8233ee67ea2d1a742ca54b010bc6ad93e
npondel <44888884+npondel@users.noreply.github.com>
2025-03-30 23:34:15 -0500

    no buffers!

--- ecowitt_relay/run.sh
+++ ecowitt_relay/run.sh
@@ -7,4 +7,4 @@ export HA_WEBHOOK_PATH=$(jq -r .ha_webhook_path /data/options.json)
 export HA_WEBHOOK_HOST=$(jq -r .ha_webhook_host /data/options.json)
 export HA_WEBHOOK_PORT=$(jq -r .ha_webhook_port /data/options.json)
 
-/usr/local/bin/python /app/ecowitt_relay.py
+/usr/local/bin/python -u /app/ecowitt_relay.py
